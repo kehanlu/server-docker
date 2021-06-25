ad632e31f47c9e79a60bd880585bcdb3235f3e91
kehanlu <me@hanklu.tw>
2021-06-25 23:15:56 +0800

    user

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM nvidia/cuda:10.2-devel-ubuntu18.04
-WORKDIR /root
+
 
 # set timezone
 ENV TZ=Asia/Taipei
@@ -7,14 +7,17 @@ RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 RUN apt-get update
 RUN apt-get install -y apt-utils
 
-
 # install some useful tools
-RUN apt-get install -y git vim wget curl zsh tmux zip htop
-
-
+RUN apt-get install -y git vim wget curl zsh tmux zip htop sudo
 
 # install pyenv dependencies
 RUN apt-get install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-RUN curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
+RUN echo '%sudo   ALL=(ALL:ALL) NOPASSWD:ALL' >> '/etc/sudoers'
+
+ARG UID
+ARG USER
+RUN useradd -r -u ${UID} -G sudo -s /bin/zsh --create-home ${USER}
+USER ${USER}
+WORKDIR /home/${USER}
 COPY init.sh /src/init.sh
--- init.sh
+++ init.sh
@@ -1,20 +1,24 @@
 # install zsh
 printf "Y\n" | sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+
 # install pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
-/root/.pyenv/bin/pyenv install 3.8.5
-/root/.pyenv/bin/pyenv virtualenv 3.8.5 lab
-/root/.pyenv/bin/pyenv global lab
+echo '\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"\neval "$(pyenv virtualenv-init -)"\n' >> .zshrc
+echo '\nexport PATH="$HOME/.local/bin:$PATH"' >> .zshrc
 
-echo 'export PATH="/root/.pyenv/bin:$PATH"\neval "$(pyenv init -)"\neval "$(pyenv virtualenv-init -)"\nexport PYENV_VIRTUALENV_DISABLE_PROMPT=1' >> .zshrc
+$HOME/.pyenv/bin/pyenv install 3.8.5
+$HOME/.pyenv/bin/pyenv virtualenv 3.8.5 lab
+$HOME/.pyenv/bin/pyenv global lab
+
+
 
 # install jupyter
-/root/.pyenv/versions/3.8.5/envs/lab/bin/python -m pip install --upgrade pip
-/root/.pyenv/versions/3.8.5/envs/lab/bin/python -m pip install jupyterlab
+$HOME/.pyenv/versions/3.8.5/envs/lab/bin/python3.8 -m pip install --upgrade pip
+$HOME/.pyenv/versions/3.8.5/envs/lab/bin/python3.8 -m pip install jupyterlab
 
-mkdir -p /root/.jupyter
+mkdir -p $HOME/.jupyter
 echo "c.ServerApp.ip = '0.0.0.0'\n\
 c.ServerApp.open_browser = False\n\
-c.ServerApp.notebook_dir = '/root'" > /root/.jupyter/jupyter_lab_config.py
+c.ServerApp.notebook_dir = '$HOME'" > $HOME/.jupyter/jupyter_lab_config.py
