6585aaa4ab4c6c211baf5abd5287d9bec884106e
kehanlu <kehanluu@gmail.com>
2021-09-10 13:25:51 +0000

    add ssh, password, args

--- Dockerfile
+++ Dockerfile
@@ -8,16 +8,25 @@ RUN apt-get update
 RUN apt-get install -y apt-utils
 
 # install some useful tools
-RUN apt-get install -y git vim wget curl zsh tmux zip htop sudo
+RUN apt-get install -y openssh-server git vim wget curl zsh tmux zip htop sudo 
 
 # install pyenv dependencies
 RUN apt-get install -y --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 RUN echo '%sudo   ALL=(ALL:ALL) NOPASSWD:ALL' >> '/etc/sudoers'
+ENV SHELL=/bin/zsh
+
+ARG DEFAULT_ENV_NAME
+ARG DEFAULT_PYTHON_VERSION
+ENV DEFAULT_ENV_NAME=${DEFAULT_ENV_NAME}
+ENV DEFAULT_PYTHON_VERSION=${DEFAULT_PYTHON_VERSION}
 
 ARG UID
 ARG USER
-RUN useradd -r -u ${UID} -G sudo -s /bin/zsh --create-home ${USER}
+ARG DEFAULT_PASSWORD
+RUN useradd -r -u ${UID} -G sudo -s /bin/zsh --create-home ${USER} 
+# Not secure!
+RUN echo "${USER}:${DEFAULT_PASSWORD}" | chpasswd 
 USER ${USER}
 WORKDIR /home/${USER}
 COPY init.sh /src/init.sh
--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
+echo `id`
+
 # install zsh
 printf "Y\n" | sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
@@ -8,15 +10,15 @@ curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer 
 echo '\nexport PYENV_ROOT="$HOME/.pyenv"\nexport PATH="$PYENV_ROOT/bin:$PATH"\neval "$(pyenv init --path)"\neval "$(pyenv virtualenv-init -)"\n' >> .zshrc
 echo '\nexport PATH="$HOME/.local/bin:$PATH"' >> .zshrc
 
-$HOME/.pyenv/bin/pyenv install 3.8.5
-$HOME/.pyenv/bin/pyenv virtualenv 3.8.5 lab
-$HOME/.pyenv/bin/pyenv global lab
+$HOME/.pyenv/bin/pyenv install $DEFAULT_PYTHON_VERSION
+$HOME/.pyenv/bin/pyenv virtualenv $DEFAULT_PYTHON_VERSION $DEFAULT_ENV_NAME
+$HOME/.pyenv/bin/pyenv global $DEFAULT_ENV_NAME
 
 
 
 # install jupyter
-$HOME/.pyenv/versions/3.8.5/envs/lab/bin/python3.8 -m pip install --upgrade pip
-$HOME/.pyenv/versions/3.8.5/envs/lab/bin/python3.8 -m pip install jupyterlab
+$HOME/.pyenv/versions/$DEFAULT_ENV_NAME/bin/python3 -m pip install --upgrade pip
+$HOME/.pyenv/versions/$DEFAULT_ENV_NAME/bin/python3 -m pip install jupyterlab
 
 mkdir -p $HOME/.jupyter
 echo "c.ServerApp.ip = '0.0.0.0'\n\
